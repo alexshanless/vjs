216c50c15c022fa9e089ff65973d788e3bccefba
alexshanless <alex.shanless@gmail.com>
2022-04-07 08:56:52 -0700

    st

---
+++ basics-assignment-1-problem/app.js
@@ -0,0 +1,9 @@
+Vue.createApp({
+  data() {
+    return {
+      name: 'Alex',
+      age: 29,
+      imgUrl: 'https://picsum.photos/200',
+    };
+  },
+}).mount('#assignment');
--- starting-code/app.js
+++ starting-code/app.js
@@ -2,6 +2,17 @@ Vue.createApp({
   data() {
     return {
       courseGoal: 'Finish the course and learn Vue',
+      vueLink: 'https://vuejs.org/',
     };
+  },
+  methods: {
+    outputGoal() {
+      const randomNumber = Math.random();
+      if (randomNumber < 0.5) {
+        return 'Learn Vue';
+      } else {
+        return 'Master Vue';
+      }
+    },
   },
 }).mount('#user-goal');
