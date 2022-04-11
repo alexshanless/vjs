5f488085de7eb940c3e7e7809589fc15d3f60ed2
alexshanless <alex.shanless@gmail.com>
2022-04-11 15:15:03 -0700

    05

---
+++ basics-05-using-the-native-event-object/app.js
@@ -0,0 +1,30 @@
+const app = Vue.createApp({
+  data() {
+    return {
+      counter: 0,
+      name: '',
+    };
+  },
+  methods: {
+    setName(event) {
+      this.name = event.target.value;
+    },
+    add(num) {
+      this.counter = this.counter + num;
+    },
+    reduce(num) {
+      this.counter = this.counter - num;
+      // this.counter--;
+    },
+  },
+  computed: {
+    fullname() {
+      if (this.name === '') {
+        return '';
+      }
+      return this.name + ' ' + Scharz;
+    },
+  },
+});
+
+app.mount('#events');
